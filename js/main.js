a7e556a7e4d1a3c2bdfdce79c2f0ac42d34e2156
hasegawa2073 <hasetatsu2001@icloud.com>
2022-08-02 23:01:45 +0900

    Add: コンテンツの削除と生成をそれぞれ関数化した

--- js/main.js
+++ js/main.js
@@ -11,46 +11,49 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const JSONImgPath = '/JSON/image.json';
 
+  // コンテンツを取り除く
+  const removeItems = function () {
+    const items = document.querySelectorAll('.item');
+    items.forEach((val) => {
+      val.remove();
+    });
+  };
+
+  // コンテンツを生成する
+  const createItems = function (val) {
+    const item = document.createElement('div');
+    item.className = 'item';
+    const img = document.createElement('img');
+    img.src = val['path'];
+    fragment.appendChild(item);
+    item.appendChild(img);
+  };
+
+  // 最初の読み込み時はすべての画像表示
   fetch(JSONImgPath)
     .then((response) => {
       return response.json();
     })
     .then((jsonData) => {
-      jsonData.forEach((val, i, array) => {
-        const item = document.createElement('div');
-        item.className = 'item';
-        const img = document.createElement('img');
-        img.src = val['path'];
-        fragment.appendChild(item);
-        item.appendChild(img);
+      jsonData.forEach((val) => {
+        createItems(val);
       });
       container.appendChild(fragment);
     });
 
   btnCorn.addEventListener('click', function () {
-    // item要素を全て消す
-    const items = document.querySelectorAll('.item');
-    items.forEach((val) => {
-      val.remove();
-    });
+    removeItems();
 
     fetch(JSONImgPath)
       .then((response) => {
         return response.json();
       })
       .then((jsonData) => {
-        const cornArray = jsonData.filter((val, i, array) => {
+        const cornArray = jsonData.filter((val) => {
           return val['type'] === 'corn';
         });
-        console.log(cornArray);
-        cornArray.forEach((val, i, array) => {
-          console.log(val);
-          const item = document.createElement('div');
-          item.className = 'item';
-          const img = document.createElement('img');
-          img.src = val['path'];
-          fragment.appendChild(item);
-          item.appendChild(img);
+        cornArray.forEach((val) => {
+          createItems(val);
         });
         container.appendChild(fragment);
       });
